196feaa9fd80c7bb0f2f2503af814c7ad19a897d
Ian Coolidge <ian.coolidge@asdnh.org>
2018-11-17 13:59:48 -0500

    more shelob

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,71 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SampleRobot;
+
+public class Robot extends SampleRobot {
+
+	WPI_TalonSRX m_leftTalon1;
+	WPI_TalonSRX m_rightTalon1;
+	WPI_TalonSRX m_leftTalon2;
+	WPI_TalonSRX m_rightTalon2;
+	
+	WPI_TalonSRX m_flyWheelL;
+	WPI_TalonSRX m_flyWheelR;
+	
+	WPI_TalonSRX m_rollers;
+	
+	Joystick m_stick = new Joystick(0);
+	
+	public void robotInit()
+	{
+		m_leftTalon1 = new WPI_TalonSRX(2);
+		m_leftTalon2 = new WPI_TalonSRX(4);
+		m_rightTalon1 = new WPI_TalonSRX(1);
+		m_rightTalon2 = new WPI_TalonSRX(3);
+		
+		m_flyWheelL = new WPI_TalonSRX(5);
+		m_flyWheelR = new WPI_TalonSRX(6);
+		
+		m_rollers = new WPI_TalonSRX(8);
+	}
+	
+	public void operatorControl()
+	{
+		while(isEnabled()) {
+			m_leftTalon1.set(ControlMode.PercentOutput, -0.5 * m_stick.getRawAxis(1));
+			m_leftTalon2.set(ControlMode.PercentOutput, -0.5 * m_stick.getRawAxis(1));
+			
+			m_rightTalon1.set(ControlMode.PercentOutput, m_stick.getRawAxis(3));
+			m_rightTalon2.set(ControlMode.PercentOutput, m_stick.getRawAxis(3));
+			
+			if(m_stick.getRawButton(1)) {
+				m_flyWheelL.set(ControlMode.PercentOutput, 0.75);
+				m_flyWheelR.set(ControlMode.PercentOutput, -0.75);
+			} else if(m_stick.getRawButton(2)) {
+				m_flyWheelL.set(ControlMode.PercentOutput, 0.5);
+				m_flyWheelR.set(ControlMode.PercentOutput, -0.5);
+			} else if(m_stick.getRawButton(3)) {
+				m_flyWheelL.set(ControlMode.PercentOutput, 0.25);
+				m_flyWheelR.set(ControlMode.PercentOutput, -0.25);
+			} else if(m_stick.getRawButton(4)) {
+				m_flyWheelL.set(ControlMode.PercentOutput, 1.0);
+				m_flyWheelR.set(ControlMode.PercentOutput, -1.0);
+			} else {
+				m_flyWheelL.set(ControlMode.PercentOutput, 0);
+				m_flyWheelR.set(ControlMode.PercentOutput, 0);
+			}
+			
+			if(m_stick.getRawButton(8)) 
+				m_rollers.set(ControlMode.PercentOutput, -0.5);
+			else if(m_stick.getRawButton(7))
+				m_rollers.set(ControlMode.PercentOutput, 0.5);
+			else
+				m_rollers.set(ControlMode.PercentOutput, 0);
+		}
+	}
+	
+}
